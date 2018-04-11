46af001f971bec408b5a4336a43d8282e40e9d26
BAFoote97 <36212656+BAFoote97@users.noreply.github.com>
2018-04-10 21:43:43 -0400

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public Rigidbody2D Player;
+
+	public float JumpForce;
+	public int JumpForceInt;
+
+	public Animator Anim;
+
+	public GameObject GameOverText;
+
+	// Use this for initialization
+	void Start () {
+
+		Player = gameObject.GetComponent<Rigidbody2D> ();
+
+		Player.mass = 1;
+		//Player.AddForce (Vector2.up * JumpForce, ForceMode2D.Impulse);
+
+	}
+
+	void UpdatePlayerPosition (Vector3 position) {
+
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			Player.AddForce (Vector2.up * JumpForce, ForceMode2D.Impulse);
+		}
+		
+	}
+}
---
+++ Assets/SpawnController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour {
+
+	public GameObject WallPrefab;
+
+	public float SpawnDelay = 1f;
+
+	public Vector3 MoveSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+		StartCoroutine (SpawnWall ());
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+
+	IEnumerator SpawnWall()
+	{
+		Instantiate (WallPrefab, transform.position, Quaternion.identity);
+
+		yield return new WaitForSeconds (SpawnDelay);
+
+		StartCoroutine (SpawnWall ());
+
+	}
+
+
+
+}
